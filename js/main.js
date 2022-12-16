8dc848b3e3a4993ff16c42acfa2f8035f02afb78
Rafael26Ananda <daffapku23@gmail.com>
2022-12-16 11:40:48 +0700

    menambahkan scrollspy pada navigasi di footer

--- js/main.js
+++ js/main.js
@@ -319,3 +319,53 @@ hash = function(h){
     location.hash = h;
   }
 }
+
+
+$('.page').css('height', $( window ).height());
+
+// scrollspy section
+(function($){
+ 
+  var sections = [];
+  var id = false;
+  var $navigasi2 = $('#navigasi-bawah a');
+  
+  $navigasi2.click(function(e){
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop: $($(this).attr('href')).offset().top
+},1000);
+    hash($(this).attr('href'));
+  });
+  
+  
+ 
+  $navigasi2.each(function(){
+    sections.push($($(this).attr('href')));
+    
+  })
+  $(window).scroll(function(e){
+   
+    var scrollTop = $(this).scrollTop() + ($(window).height()/2);
+    
+    for (var i in sections) {
+      var section = sections[i];
+      if (scrollTop > section.offset().top){
+        var scrolled_id = section.attr('id');
+      }
+    }
+    if (scrolled_id !== id) {
+      id = scrolled_id;
+      $($navigasi2).removeClass('current');
+      $('#navigasi-bawah a[href="#' + id + '"]').addClass('current'); 
+    }
+  })
+})(jQuery);
+
+hash = function(h){
+  if (history.pushState){
+    history.pushState(null, null, h);
+  }else{
+    location.hash = h;
+  }
+}
